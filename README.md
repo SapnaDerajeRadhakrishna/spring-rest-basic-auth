b334065aad515d1acd1b80ca446de9f97aba83a5
Sapna Deraje Radhakrishna <sapna.dr@gmail.com>
2019-12-26 23:04:18 -0800

    Basic HTTP authentication

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-Start Mongo Container
+To run against Mongo docker container
+
+###Start Mongo Container
 docker run -it --name my-mongo -p 27017:27017 -d mongo:latest
 
-to log into the mongo
+###to log into the mongo
 docker exec -it my-mongo bash
 
-mongo logs
+###mongo logs
 docker logs some-mongo
 
-
+###create user
 db.createUser(
 ...   {
 ...     user: "NOTiFY",
@@ -16,4 +18,10 @@ db.createUser(
 ...   }
 ... )
 
+
+
+
+##Curl commands
+1. curl -H "Authorization: Basic YWRtaW46cGFzc3dvcmQ=" http://localhost:8090/api/v1/students
+2. curl -H "Authorization: Basic YWRtaW46cGFzc3dvcmQ=" -d '{"id":"2", "name":"Sriansh", "grade":"kindergarten", "age":5}' -H "Content-Type: application/json" http://localhost:8090/api/v1/students
 
---
+++ src/main/java/org/maxwell/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package org.maxwell.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception 
+    {
+        http
+         .csrf().disable()
+         .authorizeRequests().anyRequest().authenticated()
+         .and()
+         .httpBasic();
+    }
+  
+    //pass the base64 encoding of <username>:<password> to Authorization header 
+    // example  -H "Authorization: Basic YWRtaW46cGFzc3dvcmQ=" 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) 
+            throws Exception 
+    {
+        auth.inMemoryAuthentication()
+            .withUser("admin")
+            .password("password")
+            .roles("USER");
+    }
+}
